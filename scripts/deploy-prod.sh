acb04938917f6db2e8d54205a1022d074339f38b
Davee Nguyen <davee@daveenguyen.com>
2021-05-24 03:26:36 -0700

    chore: update symantec-release version check

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -4,7 +4,11 @@ if [[ $TRAVIS_BRANCH = "master" ]]; then
         rm -rfv dist/*
         git config --global user.name "semantic-release (via TravisCI)"
         git config --global user.email "semantic-release@travis"
-        if semantic-release version --noop | grep bumped; then
+
+        CUR_VERSION=$(semantic-release print-version --current)
+        NEXT_VERSION=$(semantic-release print-version)
+
+        if [[ "$NEXT_VERSION" != "$CUR_VERSION" ]]; then
             echo "Deploying"
             semantic-release publish
             flit publish
